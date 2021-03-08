28278df30ef65263459fa0379e9b77fbc8968039
Hawkpath <hawkpathas@gmail.com>
2021-03-08 00:26:17 -0500

    Initial commit.

---
+++ WorldMapper/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WorldMapper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
