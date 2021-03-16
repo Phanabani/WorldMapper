7d20af43c6d8b7d704fd729e10763bd39d55a50f
Hawkpath <hawkpathas@gmail.com>
2021-03-16 19:54:28 -0400

    Add camera transform access.

--- WorldMapper/GameMemoryReader.cs
+++ WorldMapper/GameMemoryReader.cs
@@ -7,19 +7,33 @@ namespace WorldMapper
 {
     public class GameMemoryReader
     {
-        public Vector3 Position;
+        public Vector3 CharacterPos;
+        public Vector3 CameraPos;
+        public Matrix4x4 CameraRotMatrix;
+
+        public Matrix4x4 CameraMatrix =>
+            Matrix4x4.CreateTranslation(CameraPos) * CameraRotMatrix;
 
         private const int PROCESS_WM_READ = 0x0010;
         private Process _process;
         private IntPtr _hProcess;
 
         private byte[] _vector3Buf = new byte[4 * 3];
-        private int _posAddress;
+        private byte[] _matrix4x4Buf = new byte[4 * 16];
 
-        public GameMemoryReader(string processName, int posAddress)
+        private int _characterPosAddress;
+        private int _cameraPosAddress;
+        private int _cameraMatrixAddress;
+
+        public GameMemoryReader(
+            string processName, int characterPosAddress, int cameraPosAddress,
+            int cameraMatrixAddress
+        )
         {
             SetProcess(processName);
-            _posAddress = posAddress;
+            _characterPosAddress = characterPosAddress;
+            _cameraPosAddress = cameraPosAddress;
+            _cameraMatrixAddress = cameraMatrixAddress;
         }
 
         [DllImport("kernel32.dll")]
@@ -55,17 +69,52 @@ namespace WorldMapper
             vector.Z = BitConverter.ToSingle(_vector3Buf, 8);
         }
 
-        public void ReadPositionInfo()
+        private void ReadMatrix3x3FromMemory(int address, ref Matrix4x4 mat)
         {
-            ReadVector3FromMemory(_posAddress, ref Position);
+            int bytesRead = 0;
+            ReadProcessMemory((int)_hProcess, address, _matrix4x4Buf, _matrix4x4Buf.Length, ref bytesRead);
+
+            mat.M11 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 0);
+            mat.M12 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 1);
+            mat.M13 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 2);
+            mat.M14 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 3);
+
+            mat.M21 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 4);
+            mat.M22 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 5);
+            mat.M23 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 6);
+            mat.M24 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 7);
+
+            mat.M31 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 8);
+            mat.M32 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 9);
+            mat.M33 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 10);
+            mat.M34 = BitConverter.ToSingle(_matrix4x4Buf, 4 * 11);
+
+            mat.M41 = 0f;
+            mat.M42 = 0f;
+            mat.M43 = 0f;
+            mat.M44 = 1f;
+        }
+
+        public void ReadCharacterPos()
+        {
+            ReadVector3FromMemory(_characterPosAddress, ref CharacterPos);
+        }
+
+        public void ReadCameraPosition()
+        {
+            ReadVector3FromMemory(_cameraPosAddress, ref CameraPos);
+        }
+
+        public void ReadCameraRotation()
+        {
+            ReadMatrix3x3FromMemory(_cameraMatrixAddress, ref CameraRotMatrix);
         }
 
         public static void Main_()
         {
             GameMemoryReader reader = new GameMemoryReader(
-                "pcsx2", 0x20189EA0
+                "pcsx2", 0x20189EA0, 0x201BBD70, 0x201BE100
             );
-            reader.ReadPositionInfo();
         }
     }
 }
