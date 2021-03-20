54a4d9e0fe0b795a91759b5781c0b3a5e869581c
Hawkpath <hawkpathas@gmail.com>
2021-03-20 03:10:46 -0400

    Implement better matrix-to-euler algorithm (thank you Blender <3).

--- WorldMapper/MathUtils.cs
+++ WorldMapper/MathUtils.cs
@@ -5,6 +5,11 @@ namespace WorldMapper
 {
     public class MathUtils
     {
+        public static float Hypot(float a, float b)
+        {
+            return (float) Math.Sqrt((float) Math.Pow(a, 2) + (float) Math.Pow(b, 2));
+        }
+
         public static float[] MatrixToArray(Matrix4x4 m)
         {
             return new[]
@@ -18,14 +23,40 @@ namespace WorldMapper
 
         public static Vector3 MatrixToEuler(Matrix4x4 m)
         {
-            var euler = new Vector3();
-            euler.X = (float) Math.Atan2(m.M32, m.M33);
-            var sqrt_32_33 = (float) Math.Sqrt(
-                Math.Pow(m.M32, 2) + Math.Pow(m.M33, 2)
-            );
-            euler.Y = (float) Math.Atan2(-m.M31, sqrt_32_33);
-            euler.Z = (float) Math.Atan2(m.M21, m.M11);
-            return euler;
+            // Source: https://github.com/dfelinto/blender/blob/c4ef90f5a0b1d05b16187eb6e32323defe6461c0/source/blender/blenlib/intern/math_rotation.c#L1714
+            Vector3 eul1, eul2;
+            var cy = Hypot(m.M11, m.M12);
+
+            if (cy > 16.0f * float.Epsilon)
+            {
+                eul1.X = (float) Math.Atan2(m.M23, m.M33);
+                eul1.Y = (float) Math.Atan2(-m.M13, cy);
+                eul1.Z = (float) Math.Atan2(m.M12, m.M11);
+
+                eul2.X = (float) Math.Atan2(-m.M23, -m.M33);
+                eul2.Y = (float) Math.Atan2(-m.M13, -cy);
+                eul2.Z = (float) Math.Atan2(-m.M12, -m.M11);
+            }
+            else
+            {
+                eul1.X = (float) Math.Atan2(-m.M32, m.M22);
+                eul1.Y = (float) Math.Atan2(-m.M13, cy);
+                eul1.Z = 0;
+
+                // This is the only possible result, so return now
+                return eul1;
+            }
+
+            // https://github.com/dfelinto/blender/blob/c4ef90f5a0b1d05b16187eb6e32323defe6461c0/source/blender/blenlib/intern/math_rotation.c#L1598
+            // There is a parity issue we skipped since XYZ axis order (assumed)
+            // is of even parity, otherwise we'd negate the angles
+
+            // Source: https://github.com/dfelinto/blender/blob/c4ef90f5a0b1d05b16187eb6e32323defe6461c0/source/blender/blenlib/intern/math_rotation.c#L1761
+            // Now pick the best angles set, which is just that with the lowest
+            // absolute sum
+            var d1 = Math.Abs(eul1.X) + Math.Abs(eul1.Y) + Math.Abs(eul1.Z);
+            var d2 = Math.Abs(eul2.X) + Math.Abs(eul2.Y) + Math.Abs(eul2.Z);
+            return (d1 < d2) ? eul1 : eul2;
         }
 
         public static Matrix4x4 MatrixZUpToYUp(Matrix4x4 m)
