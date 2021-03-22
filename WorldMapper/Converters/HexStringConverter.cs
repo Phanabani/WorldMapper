5830f21432f7d76b9f5115fd84d427b6b787600e
Hawkpath <hawkpathas@gmail.com>
2021-03-22 15:34:20 -0400

    Extract HexStringConverter to its own file.

---
+++ WorldMapper/Converters/HexStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WorldMapper.Converters
+{
+    [ValueConversion(typeof(int), typeof(int))]
+    public class HexStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valueInt = value as int? ?? 0;
+            return valueInt.ToString("X");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.TryParse(value as string, NumberStyles.HexNumber, culture,
+                out var valueHex) ? valueHex : DependencyProperty.UnsetValue;
+        }
+    }
+}
--- WorldMapper/Pages/ControlsData.cs
+++ WorldMapper/Pages/ControlsData.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Windows;
-using System.Windows.Data;
-
-namespace WorldMapper.Pages
+﻿namespace WorldMapper.Pages
 {
     public class ControlsData
     {
@@ -11,21 +6,5 @@ namespace WorldMapper.Pages
         public int CameraPositionAddress { get; set; }
         public int CameraRotationAddress { get; set; }
         public float FieldOfView { get; set; } = 60;
-    }
-
-    [ValueConversion(typeof(int), typeof(int))]
-    public class HexStringConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var valueInt = value as int? ?? 0;
-            return valueInt.ToString("X");
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return int.TryParse(value as string, NumberStyles.HexNumber, culture,
-                out var valueHex) ? valueHex : DependencyProperty.UnsetValue;
-        }
     }
 }
