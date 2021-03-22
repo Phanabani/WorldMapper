3f37e3f474b153c4842dc2fcf26c09b6f4859997
Hawkpath <hawkpathas@gmail.com>
2021-03-22 17:04:52 -0400

    Add FloatRoundingConverter to round slider values.

---
+++ WorldMapper/Converters/FloatRoundingConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WorldMapper.Converters
+{
+    [ValueConversion(typeof(double), typeof(float))]
+    public class FloatRoundingConverter : IValueConverter
+    {
+        public int Digits { get; set; } = 0;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // This is the method that converts to the property
+            var valueDouble = value as double? ?? default(double);
+            var round = (float) Math.Round(valueDouble, Digits);
+            return round;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // This is the method that converts to the visual representation
+            return value as float? ?? default(float);
+        }
+    }
+}
--- WorldMapper/Converters/HexStringConverter.cs
+++ WorldMapper/Converters/HexStringConverter.cs
@@ -8,16 +8,18 @@ namespace WorldMapper.Converters
     [ValueConversion(typeof(int), typeof(string))]
     public class HexStringConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var valueInt = value as int? ?? 0;
-            return valueInt.ToString("X");
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
         {
             return int.TryParse(value as string, NumberStyles.HexNumber, culture,
                 out var valueHex) ? valueHex : DependencyProperty.UnsetValue;
+        }
+
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            var valueInt = value as int? ?? 0;
+            return valueInt.ToString("X");
         }
     }
 }
